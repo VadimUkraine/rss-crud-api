7932e550a5eded7f4570cc881f9dfbb8bda86e25
v.oleynik <v.oleynik@itransition.com>
2023-01-16 23:06:34 +0200

    refactor: clean code

--- src/server/listenSingleServer.js
+++ src/server/listenSingleServer.js
@@ -2,9 +2,6 @@ import { constants as httpStatus } from 'http2';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 import handleRequest from './handleRequest.js';
 import { handleResponse } from './handleResponse.js';
-// import responseHeaders from './response/response.json.header.js';
-// import { INTERNAL_SERVER_ERROR } from './response/response.message.js';
-// import * as logger from './handler/log.handler.js';
 import { INTERNAL_SERVER_ERROR } from '../errors/constants.js';
 
 const createNewServer = (port) => {
@@ -17,7 +14,6 @@ const createNewServer = (port) => {
 
         handleResponse(response, responseData);
       } catch (error) {
-        console.log('SHOW ERROR HERE **** listenSingleServer', error)
         response.writeHead(
           httpStatus.HTTP_STATUS_INTERNAL_SERVER_ERROR,
           { 'Content-Type': 'application/json' }
